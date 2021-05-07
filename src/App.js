7567bc64138fab0da9f3144019654e9236b1ee9b
andreafrank <andreajfrank23@gmail.com>
2021-05-07 15:31:18 -0500

    nav icons display in row on mobile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-// import * as THREE from 'three';
-// import { Canvas } from 'react-three-fiber';
-
-// import Fireflies from './components/Fireflies';
 import Navbar from './components/Navbar';
 import './App.css';
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div style={{ color: 'white' }}>
-      The contact page goes here. Check back in a day or two to contact me. :)
-    </div>
+    <>
+      contact page
+    </>
   )
 }
 
