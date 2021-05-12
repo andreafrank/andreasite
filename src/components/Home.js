b0cfdec4427e852cfd401a115216d2403234c435
andreafrank <andreajfrank23@gmail.com>
2021-05-12 15:14:14 -0500

    semantic change to title

--- src/components/Home.js
+++ src/components/Home.js
@@ -49,7 +49,7 @@ const Home = ({ characters, name }) => {
           <div className="text-6xl sm:text-8xl sm:tracking-widest">{fullList}</div>
         </div>
       </div>
-        <div className="secondary_text delay font text-center sm:ml-36 z-10">front-end developer &</div>
+        <div className="secondary_text delay font text-center sm:ml-36 z-10">web developer &</div>
         <div className="rectangle delay relative m-auto -mt-4 z-0" />
         <div className="tertiary_text delay font mb-12 text-center sm:ml-48">creative technologist</div>
         <div className="text-center px-12"><Technologies technologies={technologyList} /></div>
